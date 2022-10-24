185fe02d5116f5413f587062c38dc4ad31ba64a3
gorbatkoff <gorbatkovartyom@gmail.com>
2022-10-24 20:36:53 +0300

    Install react-router-dom and add components

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Routes, Route, Link } from 'react-router-dom';
+
+
+import Dropzone from "react-dropzone";
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import MuiAlert from '@mui/material/Alert';
+
+import axios from 'axios';
+import Archive from "./pages/Archive";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Result from "./components/Result/Result";
+
+export default function App() {
+
+  return (
+    <div className="App">
+
+      <Header />
+
+      <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/check-id:id" element={<Result />} />
+        </Routes>
+
+    </div>
+
+  );
+}
